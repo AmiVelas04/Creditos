b6ae4cd45656fb3a47024d72652ed63b0f73562f
Amk <Amk@LAPTOP>
2021-06-21 10:19:57 -0600

    rev previa a cambis

--- Arcoiris/Clases/Credito.cs
+++ Arcoiris/Clases/Credito.cs
@@ -1826,6 +1826,7 @@ namespace Arcoiris.Clases
             decimal Rint, Rcap, Rtot;
             Rcap = Math.Round((pcap - Scap), 2);
             Rint = Math.Round((pint - Sint), 2);
+            if (Rint < 0) Rint = 0;
             
             Rtot = Rcap + Rint;
             DataTable resp = new DataTable();
